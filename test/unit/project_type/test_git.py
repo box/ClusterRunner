b9395b7dcd1122c6a0cbecd32ca6cc7ea4fad880
Taejun Lee <taejun@box.com>
2014-11-04 17:57:06 -0800

    Auto-add hosts to known_hosts on git clone
    Also, users can enable strict host key checking via clusterrunner.conf
    
    Currently, we reject any host that isn't in the known_hosts file upon
    git cloning for security reasons. However, because of the new security
    measures (hmac digest validation on all requests + 600 permissions on
    the clusterrunner.conf file) we have determined this restriction is no
    longer necessary.

--- test/unit/project_type/test_git.py
+++ test/unit/project_type/test_git.py
@@ -1,3 +1,5 @@
+import pexpect
+
 from app.project_type.git import Git
 from app.util.conf.configuration import Configuration
 from test.framework.base_unit_test_case import BaseUnitTestCase
@@ -7,10 +9,13 @@ class TestGit(BaseUnitTestCase):
 
     def setUp(self):
         self.patch('app.project_type.git.fs.create_dir')
+        self.patch('os.symlink')
+        self.mock_pexpect_child = self.patch('pexpect.spawn').return_value
+        self.mock_pexpect_child.before = 'None'
+        self.mock_pexpect_child.exitstatus = 0
         super().setUp()
 
     def test_timing_file_path_happy_path(self):
-        self.patch('os.symlink')
         git_env = Git("ssh://scm.dev.box.net/box/www/current", 'origin', 'refs/changes/78/151978/27')
         timing_file = git_env.timing_file_path('QUnit')
         self.assertEquals(
@@ -20,7 +25,6 @@ class TestGit(BaseUnitTestCase):
         )
 
     def test_execute_command_in_project_specifies_cwd_if_exists(self):
-        self.patch('os.symlink')
         os_path_exists_patch = self.patch('os.path.exists')
         os_path_exists_patch.return_value = True
         project_type_popen_patch = self.patch('app.project_type.project_type.Popen')
@@ -39,7 +43,6 @@ class TestGit(BaseUnitTestCase):
         )
 
     def test_execute_command_in_project_type_specifies_cwd_if_doesnt_exist(self):
-        self.patch('os.symlink')
         os_path_exists_patch = self.patch('os.path.exists')
         os_path_exists_patch.return_value = False
         project_type_popen_patch = self.patch('app.project_type.project_type.Popen')
@@ -56,3 +59,47 @@ class TestGit(BaseUnitTestCase):
             shell=True,
             stdout=-1
         )
+
+    def test_execute_git_remote_command_auto_adds_known_host_if_prompted(self):
+        prompted = False
+
+        def expect_side_effect(*args, **kwargs):
+            nonlocal prompted
+
+            if args[0] == ['^User.*: ', '^Pass.*: ', '.*Are you sure you want to continue connecting.*'] \
+                    and not prompted:
+                prompted = True
+                return 2
+            elif args[0] == pexpect.EOF:
+                return 0
+
+            raise pexpect.TIMEOUT('some_msg')
+
+        self.mock_pexpect_child.expect.side_effect = expect_side_effect
+        Configuration['git_strict_host_key_checking'] = False
+        git = Git("some_remote_value", 'origin', 'ref/to/some/branch')
+        git._execute_git_remote_command('some_command')
+        self.mock_pexpect_child.sendline.assert_called_with("yes")
+
+    def test_execute_git_remote_command_doesnt_auto_add_known_host_if_no_prompt(self):
+        def expect_side_effect(*args, **kwargs):
+            if args[0] == ['^User.*: ', '^Pass.*: ', '.*Are you sure you want to continue connecting.*']:
+                raise pexpect.TIMEOUT('some_msg')
+            return None
+
+        self.mock_pexpect_child.expect.side_effect = expect_side_effect
+        git = Git("some_remote_value", 'origin', 'ref/to/some/branch')
+        git._execute_git_remote_command('some_command')
+        self.assertEquals(self.mock_pexpect_child.sendline.call_count, 0)
+
+    def test_execute_git_remote_command_raises_exception_if_strict_host_checking_and_prompted(self):
+        def expect_side_effect(*args, **kwargs):
+            if args[0] == ['^User.*: ', '^Pass.*: ', '.*Are you sure you want to continue connecting.*']:
+                return 2
+            return None
+
+        self.mock_pexpect_child.expect.side_effect = expect_side_effect
+        Configuration['git_strict_host_key_checking'] = True
+        git = Git("some_remote_value", 'origin', 'ref/to/some/branch')
+        self.assertRaises(RuntimeError, git._execute_git_remote_command, 'some_command')
+
